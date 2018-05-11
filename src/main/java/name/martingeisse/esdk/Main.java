d90d480cbaabd6b79641918ead15877e9554929a
Martin Geisse <martingeisse@googlemail.com>
2018-05-11 14:43:32 +0200

    initial checkin

---
+++ src/main/java/name/martingeisse/esdk/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018 Martin Geisse
+ * This file is distributed under the terms of the MIT license.
+ */
+package name.martingeisse.esdk;
+
+/**
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
