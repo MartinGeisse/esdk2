61e76b9898b5e18273d161aad673712781256389
Martin Geisse <martingeisse@googlemail.com>
2018-05-23 07:08:00 +0200

    RTL code, WIP

--- src/main/java/name/martingeisse/esdk/rtl/RtlBitSignal.java
+++ src/main/java/name/martingeisse/esdk/rtl/RtlBitSignal.java
@@ -8,4 +8,21 @@ package name.martingeisse.esdk.rtl;
  *
  */
 public interface RtlBitSignal extends RtlSignal {
+
+	default RtlBitSignal and(RtlBitSignal other) {
+		return new RtlBitOperation(getRtlItem().getDesign(), RtlBitOperation.Operator.AND, this, other);
+	}
+
+	default RtlBitSignal or(RtlBitSignal other) {
+		return new RtlBitOperation(getRtlItem().getDesign(), RtlBitOperation.Operator.OR, this, other);
+	}
+
+	default RtlBitSignal xor(RtlBitSignal other) {
+		return new RtlBitOperation(getRtlItem().getDesign(), RtlBitOperation.Operator.XOR, this, other);
+	}
+
+	default RtlBitSignal xnor(RtlBitSignal other) {
+		return new RtlBitOperation(getRtlItem().getDesign(), RtlBitOperation.Operator.XNOR, this, other);
+	}
+
 }
---
+++ src/main/java/name/martingeisse/esdk/rtl/RtlNativeModuleInstance.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2018 Martin Geisse
+ * This file is distributed under the terms of the MIT license.
+ */
+package name.martingeisse.esdk.rtl;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
+
+/**
+ *
+ */
+public abstract class RtlNativeModuleInstance {
+
+	// TODO unklar, ob das so passt
+	protected abstract ImmutableMap<String, Object> getEffectiveModuleParameters();
+	protected abstract ImmutableMap<String, RtlSignal> getEffectiveInputSignals();
+	protected abstract ImmutableMap<String, RtlSignal> getEffectiveOutputSignals();
+
+}
