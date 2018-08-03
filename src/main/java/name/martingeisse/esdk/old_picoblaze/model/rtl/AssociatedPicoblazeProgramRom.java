51b005cea817c8dc1b4e030890ba146393a107eb
Martin Geisse <martingeisse@googlemail.com>
2018-08-03 22:19:55 +0200

    RTL Picoblaze program ROM

--- src/main/java/name/martingeisse/esdk/old_picoblaze/model/rtl/AssociatedPicoblazeProgramRom.java
+++ src/main/java/name/martingeisse/esdk/old_picoblaze/model/rtl/AssociatedPicoblazeProgramRom.java
@@ -4,35 +4,49 @@
  */
 package name.martingeisse.esdk.old_picoblaze.model.rtl;
 
+import name.martingeisse.esdk.core.rtl.RtlClockNetwork;
+import name.martingeisse.esdk.core.rtl.RtlItem;
+import name.martingeisse.esdk.core.rtl.RtlRealm;
+import name.martingeisse.esdk.core.rtl.memory.RtlSynchronousRom;
+import name.martingeisse.esdk.core.rtl.signal.RtlVectorSignal;
+import name.martingeisse.esdk.core.util.vector.VectorValue;
 import name.martingeisse.esdk.old_picoblaze.model.instruction.AssociatedPicoblazeProgram;
 
 /**
- * TODO should implement, at RTL level, a synchronous ROM that loads and assembles a .psm file using
- * {@link AssociatedPicoblazeProgram} but uses the subclass of this class as the default.
+ * This is a synchronous ROM that loads and assembles a .psm from an associated resource.
+ * <p>
+ * Calling code must set the address signal before using this object.
  */
-public class AssociatedPicoblazeProgramRom {
+public class AssociatedPicoblazeProgramRom extends RtlItem {
 
-	public AssociatedPicoblazeProgramRom() {
-		this(null, null);
+	private final RtlSynchronousRom rom;
+
+	public AssociatedPicoblazeProgramRom(RtlRealm realm, RtlClockNetwork clockNetwork) {
+		this(realm, clockNetwork, null, null);
 	}
 
-	public AssociatedPicoblazeProgramRom(Class<?> anchorClass) {
-		this(anchorClass, null);
+	public AssociatedPicoblazeProgramRom(RtlRealm realm, RtlClockNetwork clockNetwork, Class<?> anchorClass) {
+		this(realm, clockNetwork, anchorClass, null);
 	}
 
-	public AssociatedPicoblazeProgramRom(String suffix) {
-		this(null, suffix);
+	public AssociatedPicoblazeProgramRom(RtlRealm realm, RtlClockNetwork clockNetwork, String suffix) {
+		this(realm, clockNetwork, null, suffix);
 	}
 
-	public AssociatedPicoblazeProgramRom(Class<?> anchorClass, String suffix) {
+	public AssociatedPicoblazeProgramRom(RtlRealm realm, RtlClockNetwork clockNetwork, Class<?> anchorClass, String suffix) {
+		super(realm);
+		rom = new RtlSynchronousRom(realm, clockNetwork, 1024, 18);
 		if (anchorClass == null) {
 			anchorClass = getClass();
 		}
 		AssociatedPicoblazeProgram program = new AssociatedPicoblazeProgram(anchorClass, suffix);
 		for (int i = 0; i < 1024; i++) {
-			int instruction = program.readInstruction(i);
+			rom.getMatrix().setRow(i, VectorValue.ofUnsigned(18, program.readInstruction(i)));
 		}
-		// TODO
+	}
+
+	public void setAddressSignal(RtlVectorSignal addressSignal) {
+		rom.setAddressSignal(addressSignal);
 	}
 
 }
