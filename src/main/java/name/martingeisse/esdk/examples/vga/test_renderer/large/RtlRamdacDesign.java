7b386459a842497970a3bdca5fa326f55c98c5a2
Martin Geisse <martingeisse@googlemail.com>
2018-09-12 23:26:52 +0200

    RAMDAC-base design, WIP

---
+++ src/main/java/name/martingeisse/esdk/examples/vga/test_renderer/large/RtlRamdacDesign.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2018 Martin Geisse
+ * This file is distributed under the terms of the MIT license.
+ */
+package name.martingeisse.esdk.examples.vga.test_renderer.large;
+
+import name.martingeisse.esdk.core.model.Design;
+import name.martingeisse.esdk.core.rtl.RtlClockNetwork;
+import name.martingeisse.esdk.core.rtl.RtlRealm;
+import name.martingeisse.esdk.core.rtl.pin.RtlInputPin;
+import name.martingeisse.esdk.core.rtl.pin.RtlOutputPin;
+import name.martingeisse.esdk.core.rtl.signal.RtlBitConstant;
+import name.martingeisse.esdk.core.rtl.signal.RtlBitSignal;
+import name.martingeisse.esdk.core.rtl.signal.RtlConcatenation;
+import name.martingeisse.esdk.core.rtl.signal.RtlVectorSignal;
+import name.martingeisse.esdk.core.rtl.synthesis.xilinx.XilinxPinConfiguration;
+import name.martingeisse.esdk.examples.vga.VgaTimer;
+import name.martingeisse.esdk.examples.vga.test_renderer.display.RtlFramebufferDisplay;
+
+/**
+ *
+ */
+public class RtlRamdacDesign extends Design {
+
+	private final RtlRealm realm;
+	private final RtlClockNetwork clock;
+	private final TestRenderer testRenderer;
+	private final RtlFramebufferDisplay display;
+	private final VgaTimer vgaTimer;
+
+	private final RtlOutputPin r;
+	private final RtlOutputPin g;
+	private final RtlOutputPin b;
+	private final RtlOutputPin hsync;
+	private final RtlOutputPin vsync;
+
+	public RtlRamdacDesign() {
+
+		int widthBits = 7;
+		int heightBits = 7;
+
+		realm = new RtlRealm(this);
+		clock = realm.createClockNetwork(new RtlBitConstant(realm, false));
+		testRenderer = new TestRenderer(realm, clock, widthBits, heightBits);
+
+		display = new RtlFramebufferDisplay(clock, widthBits, heightBits);
+		testRenderer.connectDisplay(display);
+
+		vgaTimer = new VgaTimer(clock);
+		display.setDacAddressSignal(new RtlConcatenation(realm, vgaTimer.getY().select(7, 1), vgaTimer.getX().select(7, 1)));
+
+		RtlVectorSignal dacReadData = display.getDacReadDataSignal();
+		RtlBitSignal blank = vgaTimer.getBlank().or(vgaTimer.getX().select(8)).or(vgaTimer.getX().select(9))
+			.or(vgaTimer.getY().select(8)).or(vgaTimer.getY().select(9));
+		RtlBitSignal active = blank.not();
+
+		r = vgaPin(realm, "H14", active.and(dacReadData.select(2)));
+		g = vgaPin(realm, "H15", active.and(dacReadData.select(1)));
+		b = vgaPin(realm, "G15", active.and(dacReadData.select(0)));
+		hsync = vgaPin(realm, "F15", vgaTimer.getHsync());
+		vsync = vgaPin(realm, "F14", vgaTimer.getVsync());
+
+	}
+
+	public RtlRealm getRealm() {
+		return realm;
+	}
+
+	public RtlClockNetwork getClock() {
+		return clock;
+	}
+
+	public RtlOutputPin getR() {
+		return r;
+	}
+
+	public RtlOutputPin getG() {
+		return g;
+	}
+
+	public RtlOutputPin getB() {
+		return b;
+	}
+
+	public RtlOutputPin getHsync() {
+		return hsync;
+	}
+
+	public RtlOutputPin getVsync() {
+		return vsync;
+	}
+
+	private static RtlInputPin clockPin(RtlRealm realm) {
+		XilinxPinConfiguration configuration = new XilinxPinConfiguration();
+		configuration.setIostandard("LVCMOS33");
+		RtlInputPin pin = new RtlInputPin(realm);
+		pin.setId("C9");
+		pin.setConfiguration(new XilinxPinConfiguration());
+		return pin;
+	}
+
+	private static RtlOutputPin vgaPin(RtlRealm realm, String id, RtlBitSignal outputSignal) {
+		XilinxPinConfiguration configuration = new XilinxPinConfiguration();
+		configuration.setIostandard("LVTTL");
+		configuration.setDrive(8);
+		configuration.setSlew(XilinxPinConfiguration.Slew.FAST);
+		RtlOutputPin pin = new RtlOutputPin(realm);
+		pin.setId(id);
+		pin.setConfiguration(new XilinxPinConfiguration());
+		pin.setOutputSignal(outputSignal);
+		return pin;
+	}
+
+}
