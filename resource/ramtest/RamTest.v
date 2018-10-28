b0036dc972c59bd3f48dcc475ab9c4e97af4a511
Martin Geisse <martingeisse@googlemail.com>
2018-10-28 22:48:11 +0100

    RAM test, WIP

--- resource/ramtest/RamTest.v
+++ resource/ramtest/RamTest.v
@@ -4,6 +4,20 @@
 module RamTest(
 	clockIn,
 	resetIn,
+	sdram_ck_p,
+	sdram_ck_n,
+	sdram_cke,
+	sdram_cs_n,
+	sdram_ras_n,
+	sdram_cas_n,
+	sdram_we_n,
+	sdram_ba,
+	sdram_a,
+	sdram_udm,
+	sdram_ldm,
+	sdram_udqs,
+	sdram_ldqs,
+	sdram_dq,
 	led0,
 	led1,
 	led2,
@@ -14,8 +28,27 @@ module RamTest(
 	led7
 );
 
+// clock / reset
 input clockIn;
 input resetIn;
+
+// SDRAM
+output sdram_ck_p;
+output sdram_ck_n;
+output sdram_cke;
+output sdram_cs_n;
+output sdram_ras_n;
+output sdram_cas_n;
+output sdram_we_n;
+output [1:0] sdram_ba;
+output [12:0] sdram_a;
+output sdram_udm;
+output sdram_ldm;
+inout sdram_udqs;
+inout sdram_ldqs;
+inout [15:0] sdram_dq;
+
+// LEDs
 output led0;
 output led1;
 output led2;
@@ -25,8 +58,63 @@ output led5;
 output led6;
 output led7;
 
+// clock / reset
 wire clock, clock90, clock180, clock270, reset, clockOk;
+clk_reset clk_reset1(
+	.clk_in(clockIn),
+	.reset_in(resetIn),
+	.ddr_clk_0(clock),
+	.ddr_clk_90(clock90),
+	.ddr_clk_180(clock180),
+	.ddr_clk_270(clock270),
+	.ddr_clk_ok(clockOk),
+	.reset(reset)
+);
 
+// SDRAM
+wire wbCycleStrobe;
+wire wbWriteEnable;
+wire[31:0] wbAddress;
+wire[31:0] wbWriteData;
+wire[31:0] wbReadData;
+wire wbAck;
+ddr_sdram ddr_sdram1(
+
+	// internal system signals
+	.clk0(clock),
+	.clk90(clock90),
+	.clk180(clock180),
+	.clk270(clock270),
+	.reset(~clockOk),
+
+	// internal Wishbone interface
+	.wADR_I(wbAddress[25:2]),
+	.wSTB_I(wbCycleStrobe),
+	.wWE_I(wbWriteEnable),
+	.wWRB_I(4'b1111),
+	.wDAT_I(wbWriteData),
+	.wDAT_O(wbReadData),
+	.wACK_O(wbAck),
+
+	// SDRAM signals
+	.sd_CK_P(sdram_ck_p),
+	.sd_CK_N(sdram_ck_n),
+	.sd_CKE_O(sdram_cke),
+	.sd_CS_O(sdram_cs_n),
+	.sd_RAS_O(sdram_ras_n),
+	.sd_CAS_O(sdram_cas_n),
+	.sd_WE_O(sdram_we_n),
+    .sd_A_O(sdram_a[12:0]),
+    .sd_BA_O(sdram_ba[1:0]),
+    .sd_D_IO(sdram_dq[15:0]),
+    .sd_UDM_O(sdram_udm),
+    .sd_LDM_O(sdram_ldm),
+    .sd_UDQS_IO(sdram_udqs),
+    .sd_LDQS_IO(sdram_ldqs)
+
+);
+
+// LEDs
 wire[7:0] leds;
 assign led0 = leds[0];
 assign led1 = leds[1];
@@ -37,13 +125,7 @@ assign led5 = leds[5];
 assign led6 = leds[6];
 assign led7 = leds[7];
 
-wire wbCycleStrobe;
-wire wbWriteEnable;
-wire[31:0] wbAddress;
-wire[31:0] wbWriteData;
-wire[31:0] wbReadData;
-wire wbAck;
-
+// controller
 RamTestController ramTestController1(
     .pinClock(clock),
     .pinReset(reset),
@@ -56,42 +138,4 @@ RamTestController ramTestController1(
     .pinWbAck(wbAck)
 );
 
-
-// clock / reset
-clk_reset clk_reset1(
-	.clk_in(clockIn),
-	.reset_in(resetIn),
-	.ddr_clk_0(clock),
-	.ddr_clk_90(clock90),
-	.ddr_clk_180(clock180),
-	.ddr_clk_270(clock270),
-	.ddr_clk_ok(clockOk),
-	// .clk(clk), TODO remove
-	.reset(reset)
-);
-
-// test
-reg ackReg;
-initial ackReg = 0;
-always @(posedge clock) begin
-	if (reset) begin
-		ackReg <= 0;
-	end else if (wbCycleStrobe) begin
-        ackReg <= ~ackReg;
-    end
-end
-assign wbAck = ackReg;
-reg[31:0] ram[255:0];
-reg[31:0] ramReadData;
-always @(posedge clock) begin
-    if (wbCycleStrobe) begin
-        if (wbWriteEnable) begin
-            ram[wbAddress[7:0]] <= wbWriteData;
-        end
-        ramReadData <= ram[wbAddress[7:0]];
-    end
-end
-assign wbReadData = ramReadData;
-
 endmodule
-
