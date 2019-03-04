4379f23a4fb507a3b4791b914f69bbe5e7349a34
Martin Geisse <martin.geisse@servicereisen.de>
2019-03-04 16:14:34 +0100

    cleanup

--- src/main/java/name/martingeisse/esdk/examples/vga/test_renderer/RtlRamdacDesign.java
+++ src/main/java/name/martingeisse/esdk/examples/vga/test_renderer/RtlRamdacDesign.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2018 Martin Geisse
  * This file is distributed under the terms of the MIT license.
  */
-package name.martingeisse.esdk.examples.vga.test_renderer.ramdac;
+package name.martingeisse.esdk.examples.vga.test_renderer;
 
 import name.martingeisse.esdk.core.model.Design;
 import name.martingeisse.esdk.core.rtl.RtlClockNetwork;
