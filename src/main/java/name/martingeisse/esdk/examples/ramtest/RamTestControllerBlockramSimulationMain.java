488256c10e6eaa7f8eb278b934249f888a8522b8
Martin Geisse <martin.geisse@servicereisen.de>
2019-03-05 18:21:36 +0100

    bugfix

--- src/main/java/name/martingeisse/esdk/examples/ramtest/RamTestControllerBlockramSimulationMain.java
+++ src/main/java/name/martingeisse/esdk/examples/ramtest/RamTestControllerBlockramSimulationMain.java
@@ -23,6 +23,8 @@ import name.martingeisse.esdk.core.rtl.simulation.RtlClockGenerator;
  */
 public class RamTestControllerBlockramSimulationMain {
 
+	public static final int MEMORY_ADDRESS_BITS = 10;
+
 	public static void main(String[] args) {
 
 		// design
@@ -37,19 +39,19 @@ public class RamTestControllerBlockramSimulationMain {
 		block.getStatements().when(controller.getMybusMaster().getStrobeSignal()).getThenBranch()
 			.assign(ackReg, ackReg.not());
 		controller.getMybusMaster().setAckSignal(ackReg);
-		RtlMemory ram = new RtlMemory(realm, 256, 32);
+		RtlMemory ram = new RtlMemory(realm, 1 << MEMORY_ADDRESS_BITS, 32);
 		RtlSynchronousMemoryPort ramPort = ram.createSynchronousPort(clock,
 			RtlSynchronousMemoryPort.ReadSupport.SYNCHRONOUS,
 			RtlSynchronousMemoryPort.WriteSupport.SYNCHRONOUS,
 			RtlSynchronousMemoryPort.ReadWriteInteractionMode.READ_FIRST);
 		ramPort.setClockEnableSignal(controller.getMybusMaster().getStrobeSignal());
 		ramPort.setWriteEnableSignal(controller.getMybusMaster().getWriteEnableSignal());
-		ramPort.setAddressSignal(controller.getMybusMaster().getAddressSignal().select(7, 0));
+		ramPort.setAddressSignal(controller.getMybusMaster().getAddressSignal().select(MEMORY_ADDRESS_BITS - 1, 0));
 		ramPort.setWriteDataSignal(controller.getMybusMaster().getWriteDataSignal());
 		controller.getMybusMaster().setReadDataSignal(ramPort.getReadDataSignal());
 
 		// display LEDs
-		new IntervalItem(design, 10, 1_000_000, () -> { // 10 times per simulated second
+		new IntervalItem(design, 10, 10_000_000, () -> { // 10 times per simulated second
 			System.out.println(controller.getLeds().getValue());
 		});
 
