b5ed4ea63b3db20a5edf09bedcbee30284fb8651
Martin Geisse <martin.geisse@servicereisen.de>
2019-03-08 14:12:04 +0100

    RISCV

--- src/main/java/name/martingeisse/esdk/library/riscv/InstructionLevelRiscv.java
+++ src/main/java/name/martingeisse/esdk/library/riscv/InstructionLevelRiscv.java
@@ -1,45 +1,94 @@
 package name.martingeisse.esdk.library.riscv;
 
 /**
- *
+ * Note: Interrupts are not supported for now.
  */
 public abstract class InstructionLevelRiscv {
 
 	private final int[] registers = new int[32];
-	private int pc;
+	private int pcInWords;
 
 	/**
 	 * Resets the CPU.
 	 */
 	public void reset() {
-		pc = 0;
+		pcInWords = 0;
 	}
 
 	/**
 	 * Executes a single instruction.
 	 */
 	public void step() {
-		int instruction = fetchInstruction(pc);
+		int instruction = fetchInstruction(pcInWords);
 		switch (instruction) {
 
 		}
 	}
 
-	public void interrupt() {
+	// ----------------------------------------------------------------------------------------------------------------
+	// PC
+	// ----------------------------------------------------------------------------------------------------------------
 
+	public final int getPcInWords() {
+		return pcInWords;
 	}
 
-	public int getPc() {
-		return pc;
+	public final void setPcInWords(int pcInWords) {
+		this.pcInWords = pcInWords;
 	}
 
-	public int[] getRegisters() {
-		return registers;
+	public final int getPc() {
+		return pcInWords << 2;
 	}
 
-	// TODO word or byte addresses?
-	protected abstract int fetchInstruction(int pc);
-	protected abstract int read(int address);
-	protected abstract void write(int address, int data);
+	public final void setPc(int pc) {
+		if ((pc & 3) != 0) {
+			onException(ExceptionType.INSTRUCTION_ADDRESS_MISALIGNED);
+		} else {
+			this.pcInWords = pc >> 2;
+		}
+	}
+
+	// ----------------------------------------------------------------------------------------------------------------
+	// general-purpose registers
+	// ----------------------------------------------------------------------------------------------------------------
+
+	public final int getRegister(int index) {
+		checkRegisterIndex(index);
+		return registers[index];
+	}
+
+	public final void setRegister(int index, int value) {
+		checkRegisterIndex(index);
+		if (index != 0) {
+			registers[index] = value;
+		}
+	}
+
+	private void checkRegisterIndex(int index) {
+		if (index < 0 || index >= 32) {
+			throw new RuntimeException("invalid register index: " + index);
+		}
+	}
+
+	// ----------------------------------------------------------------------------------------------------------------
+	// abstract behavior
+	// ----------------------------------------------------------------------------------------------------------------
+
+	protected abstract int fetchInstruction(int wordAddress);
+	protected abstract int read(int wordAddress);
+	protected abstract void write(int wordAddress, int data, int mask);
+
+	protected void onException(ExceptionType type) {
+		throw new RuntimeException("RISC-V cpu exception: " + type);
+	}
+
+	// ----------------------------------------------------------------------------------------------------------------
+	// helper classes
+	// ----------------------------------------------------------------------------------------------------------------
+
+	public enum ExceptionType {
+		INSTRUCTION_ADDRESS_MISALIGNED
+	}
 
 }
