22f483c47506518e7923ba654f8c4c35c0e7a327
Martin Geisse <martingeisse@googlemail.com>
2019-03-27 18:10:05 +0100

    bugfix in JAL decoding

--- src/main/java/name/martingeisse/esdk/library/riscv/InstructionLevelRiscv.java
+++ src/main/java/name/martingeisse/esdk/library/riscv/InstructionLevelRiscv.java
@@ -261,14 +261,16 @@ public abstract class InstructionLevelRiscv {
 				onException(ExceptionType.ILLEGAL_INSTRUCTION);
 				break;
 
-			case 27: // JAL
+			case 27: { // JAL
+				// instruction = imm[20], imm[10:1], imm[11], imm[19:12], rd[4:0], opcode[6:0]; implicitly imm[0] = 0
 				setRegister(instruction >> 7, pc);
-				if ((instruction & (1 << 12)) != 0) {
-					onException(ExceptionType.INSTRUCTION_ADDRESS_MISALIGNED);
-					break;
-				}
-				pc = oldPc + ((instruction >> 11) & -2);
+				int offset = ((instruction >> 11) & 0xfff00000) |
+					(instruction & 0x000ff000) |
+					((instruction >> 9) & 0x00000800) |
+					((instruction >> 20) & 0x7fe);
+				pc = oldPc + offset;
 				break;
+			}
 
 			case 28: // SYSTEM
 				onException(ExceptionType.SYSTEM_INSTRUCTION);
