71010e8d4e744c76c4ed4a2bcd28aea617f293e4
Martin Geisse <martingeisse@googlemail.com>
2019-03-30 18:32:40 +0100

    automate build process for compliance tests, WIP

---
+++ resource/riscv-test/build.php
@@ -0,0 +1,15 @@
+#!/usr/bin/env php
+<?php
+
+system('rm -rf build');
+system('mkdir build');
+system('cp src/*.txt build');
+
+foreach (scandir('src') as $filename) {
+	if (substr($filename, -2) === '.s') {
+		$basename = substr($filename, 0, strlen($filename) - 2);
+		system('riscv32-unknown-elf-gcc -c -o build/' . $basename . '.o src/' . $basename . '.s');
+        system('riscv32-unknown-elf-ld -N -Ttext 0 -o build/' . $basename . '.elf -e 0 build/' . $basename . '.o');
+        system('riscv32-unknown-elf-objcopy -j .text -I elf32-littleriscv -O binary build/' . $basename . '.elf build/' . $basename . '.bin');
+	}
+}
