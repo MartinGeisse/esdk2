0293496139f4a37e0106d3d8b404a7dcaee24320
Martin Geisse <martingeisse@googlemail.com>
2019-03-31 08:34:56 +0200

    output stored test output data instead of values from ASSERT macros

--- resource/riscv-test/build.php
+++ resource/riscv-test/build.php
@@ -5,11 +5,13 @@ system('rm -rf build');
 system('mkdir build');
 system('cp src/*.txt build');
 
+system('riscv32-unknown-elf-gcc -c -o build/common-output.o src/common/output.S');
+
 foreach (scandir('src') as $filename) {
 	if (substr($filename, -2) === '.S') {
 		$basename = substr($filename, 0, strlen($filename) - 2);
 		system('riscv32-unknown-elf-gcc -c -o build/' . $basename . '.o src/' . $basename . '.S');
-        system('riscv32-unknown-elf-ld -N -Ttext 0 -o build/' . $basename . '.elf -e 0 build/' . $basename . '.o');
+        system('riscv32-unknown-elf-ld -N -Ttext 0 -o build/' . $basename . '.elf -e 0 build/' . $basename . '.o build/common-output.o');
         system('riscv32-unknown-elf-objcopy -j .text -I elf32-littleriscv -O binary build/' . $basename . '.elf build/' . $basename . '.bin');
 	}
 }
--- resource/riscv-test/src/compliance_test.h
+++ resource/riscv-test/src/compliance_test.h
@@ -6,8 +6,13 @@
 #define RVTEST_IO_INIT
 #define RVTEST_IO_WRITE_STR(x,y)
 #define RVTEST_IO_CHECK()
-#define RV_COMPLIANCE_HALT sw x0, -36(x0)
+#define RV_COMPLIANCE_HALT j outputLoopStart
 #define RV_COMPLIANCE_CODE_END
-#define RV_COMPLIANCE_DATA_BEGIN
-#define RV_COMPLIANCE_DATA_END
-#define RVTEST_IO_ASSERT_GPR_EQ(a,b,c) sw b, -32(x0)
+
+#define RV_COMPLIANCE_DATA_BEGIN rvComplianceDataBegin:
+#define RV_COMPLIANCE_DATA_END rvComplianceDataEnd:
+#define RVTEST_IO_ASSERT_GPR_EQ(a,b,c)
+
+.global rvComplianceDataBegin
+.global rvComplianceDataEnd
+.global outputLoopStart
