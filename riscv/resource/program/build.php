e70e9fc202d33c71cbfbcbb00d68e439f4fee50c
Martin Geisse <martin.geisse@servicereisen.de>
2019-05-10 14:17:06 +0200

    test program, WIP

---
+++ riscv/resource/program/build.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+
+system('rm -rf build');
+system('mkdir build');
+
+system('riscv32-unknown-elf-gcc -c -o build/hello.o src/hello.S');
+system('riscv32-unknown-elf-ld -N -Ttext 0 -o build/program.elf -e 0 build/hello.o');
+system('riscv32-unknown-elf-objcopy -j .text -I elf32-littleriscv -O binary build/program.elf build/program.bin');
+
+/*
+foreach (scandir('src') as $filename) {
+	if (substr($filename, -2) === '.S') {
+		$basename = substr($filename, 0, strlen($filename) - 2);
+		system('riscv32-unknown-elf-gcc -c -o build/' . $basename . '.o src/' . $basename . '.S');
+        system('riscv32-unknown-elf-ld -N -Ttext 0 -o build/' . $basename . '.elf -e 0 build/' . $basename . '.o build/common-output.o');
+        system('riscv32-unknown-elf-objcopy -j .text -I elf32-littleriscv -O binary build/' . $basename . '.elf build/' . $basename . '.bin');
+	}
+}
+*/
--- riscv/src/main/java/name/martingeisse/esdk/riscv/experiment/SimulationMain.java
+++ riscv/src/main/java/name/martingeisse/esdk/riscv/experiment/SimulationMain.java
@@ -1,8 +1,12 @@
 package name.martingeisse.esdk.riscv.experiment;
 
 import name.martingeisse.esdk.core.rtl.simulation.RtlClockGenerator;
+import name.martingeisse.esdk.core.util.vector.VectorValue;
 
 import javax.swing.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  *
@@ -11,6 +15,7 @@ public class SimulationMain {
 
 	public static void main(String[] args) throws Exception {
 		Computer design = new Computer();
+		loadProgram(design.getComputerModule());
 		new RtlClockGenerator(design.getClock(), 10);
 
 		JFrame frame = new JFrame("Textmode");
@@ -21,6 +26,28 @@ public class SimulationMain {
 		frame.setVisible(true);
 
 		design.simulate();
+	}
+
+	private static void loadProgram(ComputerModule computerModule) throws Exception {
+		try (FileInputStream in = new FileInputStream("riscv/resource/program/build/program.bin")) {
+			int index = 0;
+			while (true) {
+				int first = in.read();
+				if (first < 0) {
+					break;
+				}
+				computerModule.memory0.getMatrix().setRow(index, VectorValue.of(8, first));
+				computerModule.memory1.getMatrix().setRow(index, VectorValue.of(8, readByteEofSafe(in)));
+				computerModule.memory2.getMatrix().setRow(index, VectorValue.of(8, readByteEofSafe(in)));
+				computerModule.memory3.getMatrix().setRow(index, VectorValue.of(8, readByteEofSafe(in)));
+				index++;
+			}
+		}
+	}
+
+	private static int readByteEofSafe(InputStream in) throws IOException {
+		int x = in.read();
+		return (x < 0 ? 0 : x);
 	}
 
 }
