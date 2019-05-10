0f1e110597df2e127f0ce3c9c27ddc3b4902c43b
Martin Geisse <martin.geisse@servicereisen.de>
2019-05-10 09:47:36 +0200

    multiply algorithm test

--- riscv/src/main/java/name/martingeisse/esdk/riscv/experiment/MultiplyAlgorithm.java
+++ riscv/src/main/java/name/martingeisse/esdk/riscv/experiment/MultiplyAlgorithm.java
@@ -28,15 +28,54 @@ public class MultiplyAlgorithm {
 //		return b;
 //	}
 
+//	private static long multiply(long a, long y) {
+//		long b = 0;
+//		for (int i = 0; i < 64; i++) {
+//			if ((a & 1) != 0) {
+//				b += (y << i);
+//			}
+//			a = a >> 1;
+//		}
+//		return b;
+//	}
+
+	/*
+		start:
+			AND p = a & 1
+			BEQ p = 0, label1
+			SHIFT q = y << i
+			ADD b = b + q
+		label1:
+			SHIFT a = a >> 1
+			ADD i = i + 1
+			BEQ i = 32, start
+	 */
+
 	private static long multiply(long a, long y) {
 		long b = 0;
-		for (int i = 0; i < 64; i++) {
+		if (a < 0) {
+			a = -a;
+			y = -y;
+		}
+		while (a != 0) {
 			if ((a & 1) != 0) {
-				b += (y << i);
+				b += y;
 			}
 			a = a >> 1;
+			y = y << 1;
 		}
 		return b;
 	}
 
+	/*
+		start:
+			AND p = a & 1
+			BEQ p = 0, label1
+			ADD b = b + y
+		label1:
+			SHIFT a = a >> 1
+			SHIFT y = y << 1
+			BNE a != 0, start
+
+	 */
 }
