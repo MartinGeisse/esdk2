7ffe65627e83d8c3522a4045cfe63d2b8e19ff6d
Martin Geisse <martin.geisse@servicereisen.de>
2019-05-15 09:12:52 +0200

    real terminal, WIP

--- riscv/src/main/java/name/martingeisse/esdk/riscv/experiment/Computer.java
+++ riscv/src/main/java/name/martingeisse/esdk/riscv/experiment/Computer.java
@@ -3,6 +3,10 @@ package name.martingeisse.esdk.riscv.experiment;
 import name.martingeisse.esdk.core.model.Design;
 import name.martingeisse.esdk.core.rtl.RtlClockNetwork;
 import name.martingeisse.esdk.core.rtl.RtlRealm;
+import name.martingeisse.esdk.core.rtl.pin.RtlInputPin;
+import name.martingeisse.esdk.core.rtl.pin.RtlOutputPin;
+import name.martingeisse.esdk.core.rtl.signal.RtlBitSignal;
+import name.martingeisse.esdk.core.rtl.synthesis.xilinx.XilinxPinConfiguration;
 
 /**
  *
@@ -15,8 +19,15 @@ public class Computer extends Design {
 
 	public Computer() {
 		this.realm = new RtlRealm(this);
-		this.clock = realm.createClockNetwork();
+		this.clock = realm.createClockNetwork(clockPin(realm));
 		this.computerModule = new ComputerModule(realm, clock);
+
+		vgaPin(realm, "H14", computerModule._textDisplay._vgaConnector.getR());
+		vgaPin(realm, "H15", computerModule._textDisplay._vgaConnector.getG());
+		vgaPin(realm, "G15", computerModule._textDisplay._vgaConnector.getB());
+		vgaPin(realm, "F15", computerModule._textDisplay._vgaConnector.getHsync());
+		vgaPin(realm, "F14", computerModule._textDisplay._vgaConnector.getVsync());
+
 	}
 
 	public RtlRealm getRealm() {
@@ -29,6 +40,27 @@ public class Computer extends Design {
 
 	public ComputerModule getComputerModule() {
 		return computerModule;
+	}
+
+	private static RtlInputPin clockPin(RtlRealm realm) {
+		XilinxPinConfiguration configuration = new XilinxPinConfiguration();
+		configuration.setIostandard("LVCMOS33");
+		RtlInputPin pin = new RtlInputPin(realm);
+		pin.setId("C9");
+		pin.setConfiguration(configuration);
+		return pin;
+	}
+
+	private static RtlOutputPin vgaPin(RtlRealm realm, String id, RtlBitSignal outputSignal) {
+		XilinxPinConfiguration configuration = new XilinxPinConfiguration();
+		configuration.setIostandard("LVTTL");
+		configuration.setDrive(8);
+		configuration.setSlew(XilinxPinConfiguration.Slew.FAST);
+		RtlOutputPin pin = new RtlOutputPin(realm);
+		pin.setId(id);
+		pin.setConfiguration(configuration);
+		pin.setOutputSignal(outputSignal);
+		return pin;
 	}
 
 }
--- riscv/src/main/java/name/martingeisse/esdk/riscv/experiment/SynthesisMain.java
+++ riscv/src/main/java/name/martingeisse/esdk/riscv/experiment/SynthesisMain.java
@@ -12,7 +12,6 @@ public class SynthesisMain {
 	public static void main(String[] args) throws Exception {
 		Computer design = new Computer();
 		SimulationMain.loadProgram(design.getComputerModule());
-		// design.getComputerModule().textDisplay.v
 		new ProjectGenerator(design.getRealm(), "TerminalTest", new File("ise/terminal_test"), "XC3S500E-FG320-4").generate();
 	}
 
