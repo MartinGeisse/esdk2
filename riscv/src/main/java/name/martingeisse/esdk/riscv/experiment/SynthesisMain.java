9f201dba024df6a8b8729f1055e43dd56cd548ea
Martin Geisse <martingeisse@googlemail.com>
2019-05-17 23:19:31 +0200

    investigate failure to show "hello world"

--- riscv/src/main/java/name/martingeisse/esdk/riscv/experiment/SynthesisMain.java
+++ riscv/src/main/java/name/martingeisse/esdk/riscv/experiment/SynthesisMain.java
@@ -7,6 +7,31 @@ import java.io.File;
 /**
  * Writing to the display in hardware works (see DisplayTest.mahdl). The CPU doesn't, not even with a super-simple
  * test problem that sticks to the simplest instructions and avoids the x0 register.
+ *
+ * TODO: The real CPU runs through different states than the simulated one! (A = state 0, B = state 1, ...)
+ * Real:
+ * ABBCDE
+ * ABBCDE
+ * ABBCDE
+ * ADDCI
+ * (repeats many times)
+ *
+ * Simulated:
+ * ABBCDE
+ * ABBCE
+ * ABBCE
+ * ABBCDE
+ * ABBCDE
+ * ABBCDE
+ * ABBCDE
+ * ABBCKL
+ * ABBCDE
+ * (repeats)
+ *
+ * ABBCDE is typical for OP
+ * ABBCE is typical for OP-IMM
+ * ABBCI is typical for JAL (including J)
+ * ABBCKL is typical for stores
  */
 public class SynthesisMain {
 
