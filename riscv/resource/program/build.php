d2b77f08d87aaeda37bd223ab78aa6856b06b039
Martin Geisse <martingeisse@googlemail.com>
2019-05-17 22:58:13 +0200

    investigate failure to show "hello world"

--- riscv/resource/program/build.php
+++ riscv/resource/program/build.php
@@ -4,9 +4,10 @@
 system('rm -rf build');
 system('mkdir build');
 
+system('riscv32-unknown-elf-gcc -c -o build/display_test.o src/display_test.S');
 system('riscv32-unknown-elf-gcc -c -o build/hello.o src/hello.S');
 system('riscv32-unknown-elf-gcc -c -o build/keycodes.o src/keycodes.S');
-system('riscv32-unknown-elf-ld -N -Ttext 0 -o build/program.elf -e 0 build/hello.o');
+system('riscv32-unknown-elf-ld -N -Ttext 0 -o build/program.elf -e 0 build/display_test.o');
 system('riscv32-unknown-elf-objcopy -j .text -I elf32-littleriscv -O binary build/program.elf build/program.bin');
 
 /*
--- riscv/src/main/java/name/martingeisse/esdk/riscv/experiment/SynthesisMain.java
+++ riscv/src/main/java/name/martingeisse/esdk/riscv/experiment/SynthesisMain.java
@@ -5,7 +5,8 @@ import name.martingeisse.esdk.core.rtl.synthesis.xilinx.ProjectGenerator;
 import java.io.File;
 
 /**
- *
+ * Writing to the display in hardware works (see DisplayTest.mahdl). The CPU doesn't, not even with a super-simple
+ * test problem that sticks to the simplest instructions and avoids the x0 register.
  */
 public class SynthesisMain {
 
