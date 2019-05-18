b88ac4bc3b87f4af13744efe16bdfb2d622627af
Martin Geisse <martingeisse@googlemail.com>
2019-05-18 16:24:31 +0200

    investigate failure to show "hello world"

--- riscv/src/main/java/name/martingeisse/esdk/riscv/experiment/DisplayTestSynthesisMain.java
+++ riscv/src/main/java/name/martingeisse/esdk/riscv/experiment/DisplayTestSynthesisMain.java
@@ -11,6 +11,7 @@ public class DisplayTestSynthesisMain {
 
 	public static void main(String[] args) throws Exception {
 		DisplayTestDesign design = new DisplayTestDesign();
+		DisplayTestSimulationMain.loadProgram(design.getDisplayTest());
 		new ProjectGenerator(design.getRealm(), "DisplayTest", new File("ise/display_test"), "XC3S500E-FG320-4").generate();
 	}
 
