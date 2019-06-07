bb2b6f5c255f757ad2654dff354f95b912f14aa3
Martin Geisse <martin.geisse@servicereisen.de>
2019-06-07 14:25:59 +0200

    allow debugging no-name items

--- core/src/main/java/name/martingeisse/esdk/core/model/Item.java
+++ core/src/main/java/name/martingeisse/esdk/core/model/Item.java
@@ -6,6 +6,9 @@ package name.martingeisse.esdk.core.model;
 
 import name.martingeisse.esdk.core.model.validation.ValidationContext;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 /**
  * Materialization: items are asked to materialize before simulation or synthesis. This may produce new items, which
  * are in turn asked to materialize, and so on. All this may only affect the implementation of an item, NOT the
@@ -13,12 +16,21 @@ import name.martingeisse.esdk.core.model.validation.ValidationContext;
  */
 public abstract class Item {
 
+	private static final boolean DEBUG_NONAME_ITEMS = false;
+
 	private final Design design;
 	private String name;
 
 	public Item(Design design) {
 		this.design = design;
 		design.register(this);
+		if (DEBUG_NONAME_ITEMS) {
+			Exception e = new Exception();
+			e.fillInStackTrace();
+			StringWriter w = new StringWriter();
+			e.printStackTrace(new PrintWriter(w));
+			setName(w.toString());
+		}
 	}
 
 	public final Design getDesign() {
