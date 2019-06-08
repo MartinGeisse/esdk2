c56ee8b400d8b77139168d8d86ae01637cd853de
Martin Geisse <martingeisse@googlemail.com>
2019-06-08 09:56:40 +0200

    cleanup

--- riscv/resource/program/src/main.c
+++ riscv/resource/program/src/main.c
@@ -4,105 +4,10 @@
 
 void main() {
 
-//        resetLogPointer = busWriteData[0];
-//        resetReadPointer = busWriteData[1];
-//        clearMode = busWriteData[2];
-//        loggingActive = busWriteData[3];
-
-
-
-
-
-
-/*
     // wait for SDRAM reset
     delay(500);
-
-    // log some data
-    int *basePointer = (int*)0x80000000;
-    volatile int *signalLogger = (int*)0x8000;
-    *signalLogger = 8;
-    *basePointer = 15;
-    *signalLogger = 0;
-
-    // write log to screen
-    for (int i = 0; i < 64; i++) {
-        terminalWriteHex(*signalLogger);
-        if ((i & 3) == 3) {
-            terminalWrite("\n");
-        } else {
-            terminalWrite(" ");
-        }
-    }
-*/
-
-
-
-
-
-
-
-
-
-
-
-/*
-    terminalWrite("Hello world!\n");
-    terminalWrite("This works.\n");
-    terminalWriteInt(123);
-    terminalWrite("\n");
-    terminalWriteInt(1234567890);
-    terminalWrite("\n");
-    terminalWriteHex(0xdeadbeef);
-    terminalWrite("\n");
-    terminalWriteHex(32);
-    terminalWrite("\n");
-*/
-
-
-
-
-
-
-
-
-
-
     int *basePointer = (int*)0x80000000;
 
-    // wait for SDRAM reset
-    delay(500);
-
-    int *pointer = basePointer;
-    int value = 5;
-    for (int i = 0; i < 10; i++) {
-        if ((i & 3) == 0) {
-            terminalWrite("\n");
-        }
-        terminalWriteInt(value);
-        terminalWrite(" ");
-        pointer[i] = value;
-        value += 10;
-    }
-    terminalWrite("\n\n");
-    pointer = basePointer;
-    for (int i = 0; i < 10; i++) {
-        if ((i & 3) == 0) {
-            terminalWrite("\n");
-        }
-        terminalWriteInt(pointer[i]);
-        terminalWrite(" ");
-    }
-
-
-
-
-
-
-
-
-
-    /*
     int *p1 = basePointer;
     int *p2 = basePointer + 8 * 1024 * 1024;
     int *p3 = basePointer + 16 * 1024 * 1024;
@@ -117,19 +22,5 @@ void main() {
     terminalWrite("\n");
     terminalWriteInt(*p3);
     terminalWrite("\n");
-    */
-
-    // counter to test reset
-    /*
-    terminalSetCursor(0, 0);
-    terminalWrite("                ");
-    int counter = 0;
-    while (1) {
-        terminalSetCursor(0, 0);
-        terminalWriteInt(counter);
-        counter++;
-        delay(1);
-    }
-    */
 
 }
