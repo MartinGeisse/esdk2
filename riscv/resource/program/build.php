394d5e6925e0b6042b7ca2b32866aa6676e653ca
Martin Geisse <martin.geisse@servicereisen.de>
2019-07-23 08:36:51 +0200

    removed omega mode

--- riscv/resource/program/build.php
+++ riscv/resource/program/build.php
@@ -4,7 +4,6 @@
 system('rm -rf build');
 system('mkdir build');
 
-system('riscv32-unknown-elf-gcc -msmall-data-limit=100000 -march=rv32im -mabi=ilp32 -c -o build/cpuInternal.o ../cpu-program/cpuInternal.S');
 system('riscv32-unknown-elf-gcc -msmall-data-limit=100000 -march=rv32im -mabi=ilp32 -c -o build/display_test.o src/display_test.S');
 system('riscv32-unknown-elf-gcc -msmall-data-limit=100000 -march=rv32im -mabi=ilp32 -c -o build/hello.o src/hello.S');
 system('riscv32-unknown-elf-gcc -msmall-data-limit=100000 -march=rv32im -mabi=ilp32 -c -o build/keycodes.o src/keycodes.S');
@@ -15,7 +14,7 @@ system('riscv32-unknown-elf-gcc -msmall-data-limit=100000 -march=rv32im -mabi=il
 system('riscv32-unknown-elf-gcc -msmall-data-limit=100000 -march=rv32im -mabi=ilp32 -c -o build/system.o src/system.S');
 system('riscv32-unknown-elf-gcc -msmall-data-limit=100000 -march=rv32im -mabi=ilp32 -c -o build/simdev.o src/simdev.c');
 system('riscv32-unknown-elf-gcc -msmall-data-limit=100000 -march=rv32im -mabi=ilp32 -c -o build/cpu.o src/cpu.S');
-system('riscv32-unknown-elf-ld -Map=build/program.map -A rv32im -N -Ttext 0 -o build/program.elf -e 0 build/cpuInternal.o build/start.o build/main.o build/terminal.o build/system.o build/draw.o build/simdev.o build/cpu.o');
+system('riscv32-unknown-elf-ld -Map=build/program.map -A rv32im -N -Ttext 0 -o build/program.elf -e 0 build/start.o build/main.o build/terminal.o build/system.o build/draw.o build/simdev.o build/cpu.o');
 system('riscv32-unknown-elf-objcopy -j .text -j .rodata -j .sdata -I elf32-littleriscv -O binary build/program.elf build/program.bin');
 
 /*
