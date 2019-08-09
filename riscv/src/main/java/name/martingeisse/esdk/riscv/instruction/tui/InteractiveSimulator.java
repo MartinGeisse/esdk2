0a05f2e8193119e04296e08fe2c5ca2ad4dc63ff
Martin Geisse <martin.geisse@servicereisen.de>
2019-08-09 13:44:02 +0200

    highlevel simulator, WIP

--- riscv/src/main/java/name/martingeisse/esdk/riscv/instruction/tui/InteractiveSimulator.java
+++ riscv/src/main/java/name/martingeisse/esdk/riscv/instruction/tui/InteractiveSimulator.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2018 Martin Geisse
  * This file is distributed under the terms of the MIT license.
  */
-package name.martingeisse.esdk.riscv.instruction.ui;
+package name.martingeisse.esdk.riscv.instruction.tui;
 
 import name.martingeisse.esdk.riscv.instruction.InstructionLevelRiscv;
 
