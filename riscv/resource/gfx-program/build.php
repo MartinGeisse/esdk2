c765762abe95d326e7f40a8cb7f5600f26d9dc46
Martin Geisse <martin.geisse@servicereisen.de>
2019-09-10 15:55:35 +0200

    GFX program: build script refactoring

--- riscv/resource/gfx-program/build.php
+++ riscv/resource/gfx-program/build.php
@@ -1,16 +1,45 @@
 #!/usr/bin/env php
 <?php
 
+define('TOOL', '~/riscv-toolchain/bin/riscv32-unknown-elf-');
+
+$objectFiles = array();
+
+function buildFile($inputPath, $outputPath) {
+    global $objectFiles;
+    $objectFiles[] = $outputPath;
+    system(TOOL . 'gcc -msmall-data-limit=100000 -march=rv32im -mabi=ilp32 -c -o ' . $outputPath . ' ' . $inputPath);
+}
+
+function linkFiles() {
+    global $objectFiles;
+    $objectFilesList = implode(' ', $objectFilesList);
+    system(TOOL . 'ld -Map=build/program.map -A rv32im -N -Ttext 0 -o build/program.elf -e 0 ' . objectFilesList);
+}
+
+function convertExecutable() {
+    system(TOOL . 'objcopy -j .text -j .rodata -j .sdata -I elf32-littleriscv -O binary build/program.elf build/program.bin');
+}
+
+//
+// --------------------------------------------------------------------------------------------------------------------
+//
+
 system('rm -rf build');
 system('mkdir build');
 
-system('~/riscv-toolchain/bin/riscv32-unknown-elf-gcc -msmall-data-limit=100000 -march=rv32im -mabi=ilp32 -c -o build/start.o src/start.S');
-system('~/riscv-toolchain/bin/riscv32-unknown-elf-gcc -msmall-data-limit=100000 -march=rv32im -mabi=ilp32 -c -o build/main.o src/main.c');
-system('~/riscv-toolchain/bin/riscv32-unknown-elf-gcc -msmall-data-limit=100000 -march=rv32im -mabi=ilp32 -c -o build/draw.o src/draw.c');
-system('~/riscv-toolchain/bin/riscv32-unknown-elf-gcc -msmall-data-limit=100000 -march=rv32im -mabi=ilp32 -c -o build/system.o src/system.S');
-system('~/riscv-toolchain/bin/riscv32-unknown-elf-gcc -msmall-data-limit=100000 -march=rv32im -mabi=ilp32 -c -o build/simdev.o src/simdev.c');
-system('~/riscv-toolchain/bin/riscv32-unknown-elf-gcc -msmall-data-limit=100000 -march=rv32im -mabi=ilp32 -c -o build/cpu.o src/cpu.S');
-system('~/riscv-toolchain/bin/riscv32-unknown-elf-gcc -msmall-data-limit=100000 -march=rv32im -mabi=ilp32 -c -o build/vec3test.o src/vec3test.cpp');
-system('~/riscv-toolchain/bin/riscv32-unknown-elf-gcc -msmall-data-limit=100000 -march=rv32im -mabi=ilp32 -c -o build/engine.o src/engine.cpp');
-system('~/riscv-toolchain/bin/riscv32-unknown-elf-ld -Map=build/program.map -A rv32im -N -Ttext 0 -o build/program.elf -e 0 build/start.o build/main.o build/system.o build/draw.o build/simdev.o build/cpu.o');
-system('~/riscv-toolchain/bin/riscv32-unknown-elf-objcopy -j .text -j .rodata -j .sdata -I elf32-littleriscv -O binary build/program.elf build/program.bin');
+$paths = array(
+    'src/system/start.S' => 'build/start.o',
+    'src/system/draw.o' => 'build/draw.c',
+    'src/system/system.o' => 'build/system.S',
+    'src/system/simdev.o' => 'build/simdev.c',
+    'src/system/cpu.o' => 'build/cpu.S',
+    'src/main.o' => 'build/main.c',
+    'src/vec3dtest.o' => 'build/vec3dtest.cpp',
+    'src/engine.o' => 'build/engine.cpp',
+);
+foreach ($paths as $inputPath => $outputPath) {
+    buildFile($inputPath, $outputPath);
+}
+linkFiles();
+convertExecutable();
