a33c3ddc09bdacf5aa63df076451a0c26bc51057
Martin Geisse <martingeisse@googlemail.com>
2020-04-18 10:24:30 +0200

    bugfix: sqrt algorithm cannot use hardware division since it does not exist yet

---
+++ riscv/resource/gfx-program/src/system/cpu.c
@@ -0,0 +1,24 @@
+
+#include "cpu.h"
+
+unsigned int udivrem(unsigned int x, unsigned int y, int rem) {
+    unsigned int quotient = 0, remainder = 0;
+    for (int i = 0; i < 32; i++) {
+        remainder = (remainder << 1) | (x >> 31);
+        x = x << 1;
+        quotient = (quotient << 1);
+        if (remainder >= y) {
+            remainder -= y;
+            quotient++;
+        }
+    }
+    return rem ? remainder : quotient;
+}
+
+unsigned int udiv(unsigned int x, unsigned int y) {
+    return udivrem(x, y, 0);
+}
+
+unsigned int urem(unsigned int x, unsigned int y) {
+    return udivrem(x, y, 1);
+}
