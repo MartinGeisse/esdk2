9e97c0ea879bd4f334dbf44cda27672501d15213
Martin Geisse <martingeisse@googlemail.com>
2020-08-07 07:15:04 +0200

    in gradle, turn project dependencies into mahdlCodegen task dependencies

--- mahdl/mahdl-gradle/src/main/groovy/name/martingeisse/mahdl/gradle/MahdlGradlePlugin.groovy
+++ mahdl/mahdl-gradle/src/main/groovy/name/martingeisse/mahdl/gradle/MahdlGradlePlugin.groovy
@@ -1,7 +1,11 @@
 package name.martingeisse.mahdl.gradle
 
+
+import org.gradle.BuildAdapter
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.ProjectDependency
+import org.gradle.api.invocation.Gradle
 
 /**
  * Debugging: ./gradlew clean build -Dorg.gradle.debug=true --no-daemon
@@ -12,17 +16,43 @@ import org.gradle.api.Project
  */
 class MahdlGradlePlugin implements Plugin<Project> {
 
-	void apply(Project project) {
-		MahdlCodegenTask task = project.tasks.create("mahdlCodegen", MahdlCodegenTask.class);
-		task.group = 'build';
-		task.description = 'Generates Java code from MaHDL sources.';
-		task.sourceDirectory = new File(project.projectDir, "src/mahdl");
-		task.outputDirectory = new File(project.getBuildDir(), "mahdl-java");
-		project.tasks.compileJava.dependsOn(task);
-		project.sourceSets.main.java.srcDirs += task.outputDirectory;
-		project.sourceSets.main.resources.srcDirs += task.outputDirectory;
-		project.sourceSets.test.java.srcDirs += task.outputDirectory;
-		project.sourceSets.test.resources.srcDirs += task.outputDirectory;
-	}
+    void apply(Project project) {
+
+        // create the mahdlCodegen task
+        MahdlCodegenTask task = project.tasks.create("mahdlCodegen", MahdlCodegenTask.class);
+        task.group = "build";
+        task.description = "Generates Java code from MaHDL sources.";
+        task.sourceDirectory = new File(project.projectDir, "src/mahdl");
+        task.outputDirectory = new File(project.buildDir, "mahdl-java");
+
+        // mahdlCodegen must run before compiling Java code
+        project.tasks.compileJava.dependsOn(task);
+
+        // add the task's output folders as Java source folders
+        project.sourceSets.main.java.srcDirs += task.outputDirectory;
+        project.sourceSets.main.resources.srcDirs += task.outputDirectory;
+        project.sourceSets.test.java.srcDirs += task.outputDirectory;
+        project.sourceSets.test.resources.srcDirs += task.outputDirectory;
+
+        // Turn project dependencies into task dependencies. We have to delay this until the end of the configuration
+        // phase because project dependencies are not fully known until then.
+        project.gradle.addBuildListener(new BuildAdapter() {
+            @Override
+            void projectsEvaluated(Gradle gradle) {
+                project.configurations.each { configuration ->
+                    configuration.dependencies.each { dependency ->
+                        task.dependencyOutputs += dependency
+                        if (dependency instanceof ProjectDependency) {
+                            def producerProject = ((ProjectDependency) dependency).dependencyProject
+                            if (producerProject.tasks.hasProperty("jar")) {
+                                task.dependsOn(producerProject.tasks.jar)
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+    }
 
 }
